6558d83d0fcb039bd78b45f56d2eb085f1f67f22
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2025-08-04 16:39:55 -0600

    CMake basic setup

---
+++ src/RegisterExtension.cpp
@@ -0,0 +1,52 @@
+// Copied from godot-cpp/test/src and modified.
+
+#include "gdextension_interface.h"
+
+#include "godot_cpp/core/class_db.hpp"
+#include "godot_cpp/core/defs.hpp"
+#include "godot_cpp/godot.hpp"
+
+#include "TRMesh/test.hpp"
+
+namespace
+{
+   void initializeExtension(godot::ModuleInitializationLevel p_level)
+   {
+      if (p_level != godot::MODULE_INITIALIZATION_LEVEL_SCENE)
+      {
+         return;
+      }
+
+      godot::print_line("Loading TRMeshGD (Build Time: ", __DATE__, " ", __TIME__, ")");
+      godot::print_line("TEST: ", test());
+   }
+
+   void uninitializeExtension(godot::ModuleInitializationLevel p_level)
+   {
+      if (p_level != godot::MODULE_INITIALIZATION_LEVEL_SCENE)
+      {
+         return;
+      }
+   }
+}
+
+extern "C"
+{
+   // Plugin entry point
+   GDExtensionBool GDE_EXPORT GDExtensionInit(
+       GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library,
+       GDExtensionInitialization *r_initialization)
+   {
+      {
+         godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library,
+                                                        r_initialization);
+
+         init_obj.register_initializer(initializeExtension);
+         init_obj.register_terminator(uninitializeExtension);
+         init_obj.set_minimum_library_initialization_level(
+             godot::MODULE_INITIALIZATION_LEVEL_SCENE);
+
+         return init_obj.init();
+      }
+   }
+}
